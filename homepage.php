2c7fa73f571b6bae7337c0dcf93792a76301eefe
dajahenry <42515070+dajahenry@users.noreply.github.com>
2018-10-23 23:52:52 -0400

    updated php

--- homepage.php
+++ homepage.php
@@ -1,2 +1,17 @@
 <?php
-echo "Today is" . date("Y/m/d") . "<br>";
+echo "Today is" . date("Y/m/d");
+
+$d1 = strtotime("February 14");
+$d2= ceil(($d1-time())/60/60/24);
+echo "There are" . $d2 . "days until my birthday.";
+
+$numbers [0, 1, 2, 3, 4, 5]
+function squared_sum ($numbers) {
+	$sum = 0;
+	foreach ($numbers as $number) {
+		$sum += $number* $number;}
+		return $sum
+		# code...
+	}
+}
+?>;
